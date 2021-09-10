509a9953df4e3f4b11d42bf8794f17648a17c6fb
Alex White <parzivalwhite@gmail.com>
2021-09-10 14:34:56 -0700

    front end logic for location change

--- api/routes/unicorn.js
+++ api/routes/unicorn.js
@@ -7,6 +7,16 @@ let data = JSON.parse(fs.readFileSync('unicorns.json'), 'utf8');
 
 router.get("/", function(req, res, next) {
     res.send(data);
-});
+})
+
+router.post("/", function(req, res, next) {
+    let data = JSON.stringify(req.body);
+    console.log('data', data, typeof(data));
+    fs.writeFile('unicorns.json', data, (err) => {
+        if (err) console.log('err', err);
+        console.log('Written to the file');
+    })
+    // res.status(200)
+})
 
 module.exports = router;
--- cliennt/src/App.js
+++ cliennt/src/App.js
@@ -5,24 +5,50 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {unicorns: [{ name: ''}] }
+
+    this.unicornUpdater = this.unicornUpdater.bind(this);
+    this.unicornSort = this.unicornSort.bind(this);
+    this.updateLocation = this.updateLocation.bind(this);
   }
+
+  updateLocation(e) {
+    let name = e.target.previousSibling.data;
+    let value = e.target.value;
+    console.log('name', name, 'value', value);
+  }
+
   getUnicorns() {
     return fetch('http://localhost:5000/unicorn')
       .then(res => res.json())
       .catch(err => err);
   }
+
+  unicornUpdater() {
+    let data = this.state;
+    console.log('I am happening')
+    fetch('http://localhost:5000/unicorn', 
+    {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(data)}
+    )
+  }
+
   async componentWillMount() {
     let herd = await this.getUnicorns();
     this.setState({ unicorns: herd.unicorns})
   }
 
   // Sorts the unicorns based off location
-  unicornSort(arr, str) {
+  unicornSort(str) {
     return (
       <ul>
-        { arr.map((unicorn) => {
+        { this.state.unicorns.map((unicorn) => {
           if( unicorn.location === str) {
-            return (<li id={unicorn.name}>{unicorn.name}</li>)
+            return (<li id={unicorn.name}>{unicorn.name} 
+                <select onChange={this.updateLocation}>
+                  <option value='Barn'>Barn</option>
+                  <option value='Pasture'>Pasture</option>
+                  <option value='Trail'>Trail</option>
+                </select>
+              </li>)
           } return <></>
         })}
       </ul>
@@ -30,15 +56,15 @@ class App extends Component {
   }
 
   render() {
-    let listOfUnicorns = this.state.unicorns;
     return(
       <>
       <h3>Barn</h3>
-        {this.unicornSort(listOfUnicorns, 'Barn')}
+        {this.unicornSort('Barn')}
       <h3>Pasture</h3>
-      {this.unicornSort(listOfUnicorns, 'Pasture')}
+      {this.unicornSort('Pasture')}
       <h3>Trail</h3>
-      {this.unicornSort(listOfUnicorns, 'Trail')}
+      {this.unicornSort('Trail')}
+      <button onClick={this.unicornUpdater}>Update me</button>
       </>
     )
   }
